24e96c9231422869e09b3af18b16e6bb3b334e55
Michael Marconi <michaelziga2@gmail.com>
2024-04-25 16:02:27 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -10,7 +10,11 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  next()
+  if (req.session.user){
+    next()
+  } else {
+    next({ status: 401, message: "You shall not pass!" })
+  }
 }
 
 /*
