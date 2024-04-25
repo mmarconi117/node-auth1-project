9a04e429cb474943a6800834e7af7965b919cc6d
Michael Marconi <michaelziga2@gmail.com>
2024-04-25 15:37:10 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require("../api/users/users-router");
 const authRouter = require("../api/auth/auth-router");
+const session = require("express-session");
+const Store = require("connect-session-knex")(session);
+const knex = require("../data/db-config");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -19,6 +22,26 @@ const authRouter = require("../api/auth/auth-router");
 
 const server = express();
 
+server.use(
+  session({
+    name: "chocolatechip",
+    secret: "keep it secret, keep it safe",
+    resave: false,
+    saveUninitialized: false,
+    store: new Store({
+      knex,
+      createTable: true,
+      clearInterval: 1000 * 60 * 60,
+      tablename: "sessions",
+      sidfieldname: "sid",
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+      httpOnly: true,
+    },
+  })
+);
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
